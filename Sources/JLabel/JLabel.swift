ee0ad4cbea4b573efc03cc0fbf6e9eb095c36e2d
jjunhaa0211 <goodjunha@gmail.com>
2024-08-07 14:54:30 +0900

    커밋

--- Sources/JLabel/JLabel.swift
+++ Sources/JLabel/JLabel.swift
@@ -1,2 +1,96 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import UIKit
+import JUtile
+import Then
+import SnapKit
+
+import RxSwift
+import RxCocoa
+
+open class JNLabel: BaseLabel {
+
+    private var textLabel = BaseLabel()
+
+    public init(
+        text: String? = "",
+        font: UIFont? = UIFont.Pretendard.titleLarge,
+        textColor: UIColor? = .black,
+        isCenter: Bool? = true,
+        numberOfLineCount: Int = 1
+    ) {
+        super.init(frame: .zero)
+        
+        setupUI(text: text,
+                font: font,
+                textColor: textColor,
+                isCencter: isCenter ?? true,
+                numberOfLineCount: numberOfLineCount
+        )
+    }
+
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupUI(text: String?, font: UIFont?, textColor: UIColor?, isCencter: Bool, numberOfLineCount: Int) {
+        if isCencter == true {
+            textLabel.setTextAlignmentAndNumberOfLines(alignment: .center)
+        } else {
+            textLabel.setTextAlignmentAndNumberOfLines(alignment: .left)
+        }
+
+        textLabel.text = text
+        textLabel.setColor(textColor: textColor)
+
+        if let font = font {
+            textLabel.setPretendardFont(font: font)
+        }
+
+        textLabel.setTextAlignmentAndNumberOfLines(numberOfLines: numberOfLineCount)
+        
+        textLabel.lineBreakMode = .byCharWrapping
+    }
+
+    public func changeText(text: String?) {
+        textLabel.text = text
+    }
+    
+    public func changeFont(font: UIFont?) {
+        textLabel.font = font
+    }
+
+    public func changeTextColor(color: UIColor?) {
+        textLabel.textColor = color
+    }
+
+    public override func layout() {
+        super.layout()
+
+        self.addSubview(textLabel)
+
+        self.textLabel.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+
+        self.snp.makeConstraints {
+            switch textLabel.font {
+            case UIFont.Pretendard.titleLarge:
+                $0.height.equalTo(48)
+            case UIFont.Pretendard.bodyMedium:
+                $0.height.equalTo(50)
+            case UIFont.Pretendard.bodyMedium2:
+                $0.height.equalTo(20)
+                $0.width.equalTo(280.0)
+            case UIFont.Pretendard.light:
+                $0.height.equalTo(64.0)
+            case UIFont.Pretendard.bodyLarge:
+                $0.height.equalTo(24.0)
+            case UIFont.Pretendard.titleMedium:
+                $0.height.equalTo(32.0)
+            case UIFont.Pretendard.labelMedium:
+                $0.height.equalTo(20.0)
+            default:
+                break
+            }
+        }
+    }
+}
--- Sources/JLabel/UIFont+.swift
+++ Sources/JLabel/UIFont+.swift
@@ -1,8 +1,69 @@
-//
-//  File.swift
-//  
-//
-//  Created by 박준하 on 8/7/24.
-//
+import UIKit
 
-import Foundation
+public extension UIFont {
+    struct Pretendard {
+         @nonobjc public static var headlineLarge: UIFont {
+             return UIFont.systemFont(ofSize: 64)
+         }
+         
+         @nonobjc public static var headlineMedium: UIFont {
+             return UIFont.systemFont(ofSize: 48)
+         }
+        
+        @nonobjc public static var headlineSmall: UIFont {
+            return UIFont.systemFont(ofSize: 40)
+        }
+        
+        @nonobjc public static var titleLarge: UIFont {
+            return UIFont.systemFont(ofSize: 36)
+        }
+        
+        @nonobjc public static var titleMedium: UIFont {
+            return UIFont.systemFont(ofSize: 24)
+        }
+        
+        @nonobjc public static var titleSmall: UIFont {
+            return UIFont.systemFont(ofSize: 16)
+        }
+        
+        @nonobjc public static var bodyLarge: UIFont {
+            return UIFont.systemFont(ofSize: 20)
+        }
+        
+        @nonobjc public static var bodyMedium: UIFont {
+            return UIFont.systemFont(ofSize: 16)
+        }
+        
+        @nonobjc public static var bodyMedium2: UIFont {
+            return UIFont.systemFont(ofSize: 18)
+        }
+        
+        @nonobjc public static var bodySmall: UIFont {
+            return UIFont.systemFont(ofSize: 14)
+        }
+        
+        @nonobjc public static var bodyTiny: UIFont {
+            return UIFont.systemFont(ofSize: 12)
+        }
+        
+        @nonobjc public static var labelLarge: UIFont {
+            return UIFont.systemFont(ofSize: 20)
+        }
+        
+        @nonobjc public static var labelMedium: UIFont {
+            return UIFont.systemFont(ofSize: 16)
+        }
+        
+        @nonobjc public static var labelSmall: UIFont {
+            return UIFont.systemFont(ofSize: 14)
+        }
+        
+        @nonobjc public static var light: UIFont {
+            return UIFont.systemFont(ofSize: 64)
+        }
+        
+        @nonobjc public static var light32: UIFont {
+            return UIFont.systemFont(ofSize: 32)
+        }
+     }
+}
